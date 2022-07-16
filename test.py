26aaf673e32601bca09a47a8c4c21c8c0d69dc08
Sait <saimeku@hotmail.com>
2022-07-16 16:59:36 +0200

    a

---
+++ test.py
@@ -0,0 +1,17 @@
+from flask import request, Flask
+
+app = Flask(__name__)
+
+@app.route('/login')
+def hello_world2():
+    error = None
+    name =    request.args.get('name')
+    return "hallo"+name
+
+@app.route('/')
+def hello_world():
+    return "hallo"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='8000', debug=True)
+

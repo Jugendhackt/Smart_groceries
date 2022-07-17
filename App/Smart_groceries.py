14d2047fe97b47e223621ffeda2c8b0561c24b63
CorinnaJugendhackt <44921252+CorinnaJugendhackt@users.noreply.github.com>
2022-07-17 08:02:42 +0200

    App Framework and Design

--- App/Smart_groceries.py
+++ App/Smart_groceries.py
@@ -1,14 +1,27 @@
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.app import App
 from kivy.lang.builder import Builder
+from kivy.properties import ObjectProperty
+import requests
 
 
 class StartScreen(Screen):
-    pass
+    first_name = ObjectProperty(None)
+    last_name = ObjectProperty(None)
+
+    def btn(self):
+        print("Name:", self.first_name.text, "Last Name:", self.last_name.text)
+        name = self.first_name.text
+        last_name = self.last_name.text
+        dic = {"Namen": name, "Nachname": last_name}
+        requests.post("http://127.0.0.1:8000/Name_hinzu", dic)
 
 
 class GroceryList(Screen):
-    pass
+    product_name = ObjectProperty(None)
+
+    def btn(self):
+        print("Product Name:", self.product_name.text)
 
 
 class ShoppingCart(Screen):
